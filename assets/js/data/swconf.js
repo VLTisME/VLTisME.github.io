52b04cda9963987ef495342b9ec4993fdbe3bf41
VLTisME <VLTisME@users.noreply.github.com>
2025-02-28 03:23:18 +0000

    deploy: 40ef2afed04a9a190829194fc13cfd9d2879f046

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1740711916',resources: [
+    cacheName: 'chirpy-1740712998',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
